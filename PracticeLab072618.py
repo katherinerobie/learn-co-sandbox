2282c9f43cdb58c0608b539dc3af4b29a61a28ec
Katherine Robie <katherine.b.robie@gmail.com>
2018-08-28 15:23:08 +0000

    automatically backed up by learn

---
+++ PracticeLab072618.py
@@ -0,0 +1,27 @@
+# 1. Grab the number 6 from this nested_list and set it equal to the number_six variable
+
+nested_list = [1, [2, [4, [5, [6]], 3]]]
+number_six = None
+
+
+# 2. Sort the cats list in alphabetical order
+cats = ['Maine Coon', 'Tabby', 'Siamese', 'Garfield', 'Sylvester']
+
+
+# 3. Look up a new string or list method and explain how it works
+
+# 4. Create a new list cat_jrs by appending 'Jr.' to each of the cats names
+
+# 5. create a pets array that contains the elements of the cats and dogs lists
+
+cats = ['Maine Coon', 'Tabby', 'Siamese', 'Garfield', 'Sylvester']
+dogs = ['Scooby', 'Scrappy', 'Clifford', 'Pickles', 'Floyd']
+animals =
+
+# 6. add the cats in more_cats to the original list of cats
+
+cats = ['Maine Coon', 'Tabby', 'Siamese', 'Garfield', 'Sylvester']
+more_cats = ['Horatio', 'Whiskers', 'Chesire']
+
+# BONUS: Sorts the cats list in alphabetical order by third character of each element
+cats = ['Maine Coon', 'Tabby', 'Siamese', 'Garfield', 'Sylvester']
